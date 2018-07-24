751e65d82c0b7cd638961c7aed21f3257ab930e6
Kathleen Eife <keife218@icloud.com>
2018-07-24 19:43:31 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,24 @@
+def counter 
+  def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 != 0
+     "Fizz"
+    
+  elsif number % 5 == 0 && number % 3 != 0
+   "Buzz"
+  
+  elsif number % 3 == 0 && number % 5 == 0 
+   "FizzBuzz"
+  
+  elsif number % 3 != 0 || number % 5 != 0
+   nil 
+end
+end
+
+  number = 0
+  while number < 11
+    number 
+    number += 1
+  end
+end
+
+counter
